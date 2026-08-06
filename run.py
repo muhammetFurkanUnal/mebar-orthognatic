401a12735db1057a903c395dc393987ee47dc759
furkan <furkan@furkan-MacBook-Air-6.local>
2026-08-06 14:08:57 +0300

    all promts ready

--- run.py
+++ run.py
@@ -5,24 +5,24 @@ import json
 
 if __name__ == "__main__":
 	# import system prompt
-	system_prompt_path = "prompts/educated_profile.xml"
+	system_prompt_path = "prompts/uneducated_profile.xml"
 	with open(system_prompt_path, "r") as file:
 		system_prompt = file.read()
 
 	response_json = llm_call(
 		base_url=config.base_url,
-		model_name='google/gemini-3.1-pro-preview',
+		model_name='anthropic/claude-opus-4.8',
 		api_key=config.API_KEY_OPENROUTER,
 		system_prompt=system_prompt,
 		image_paths=["assets/sample.png"],
-		file_paths=["assets/Arnett part I.pdf", "assets/Arnett part II.pdf"]
+		# file_paths=["assets/Arnett part I.pdf", "assets/Arnett part II.pdf"]
 	)
 
 	print(response_json)
 
 	# save output .json
 	output_folder = "outputs"
-	output_file = "gemini_3_1_pro_prev"
+	output_file = "uned_opus_4_8"
 	os.makedirs(output_folder, exist_ok=True)
 	output_path = f"{output_folder}/{output_file}.json"
 
